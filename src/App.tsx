8e316c99ecf844c068e839f95a378fc90960a33e
abui <adjiem31@gmail.com>
2024-05-11 16:24:43 +0700

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import LoginPage from './page/login';
 import IndexPage from './page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const router = createBrowserRouter([
   {
     path: '/login',
@@ -12,8 +16,15 @@ const router = createBrowserRouter([
     element: <IndexPage />,
   },
 ]);
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
